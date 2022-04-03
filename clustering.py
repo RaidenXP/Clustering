bfee7513d3e974262977b03ace26193fa1c0a4fb
RaidenXP <nathanthientran@gmail.com>
2022-04-03 16:51:51 -0700

    first commit

---
+++ clustering.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+# DO NOT CHANGE THE FOLLOWING LINE
+def lloyds(data, k, columns, centers=None, n=None, eps=None):
+# DO NOT CHANGE THE PRECEDING LINE
+    # This function has to return a list of k cluster centers (lists of floats of the same length as columns)
+    pass
+    
+# DO NOT CHANGE THE FOLLOWING LINE
+def kmedoids(data, k, distance, centers=None, n=None, eps=None):
+# DO NOT CHANGE THE PRECEDING LINE
+    # This function has to return a list of k cluster centroids (data instances!)
+    pass
+
