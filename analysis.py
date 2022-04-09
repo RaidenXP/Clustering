6d03e46d73908c2aedba6f4009aace95e957c329
RaidenXP <nathanthientran@gmail.com>
2022-04-08 22:07:33 -0700

    added kmedoids and finishing up

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import clustering
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+import ast
 
 def plot(data, centers, x, y, scale=None):
     def dist(point,center):
@@ -38,17 +39,134 @@ def plot(data, centers, x, y, scale=None):
     plt.ylim((fr,to))
     plt.show()
 
+def analysis_lloyds():
+    def dist(center, instance, columns):
+        distance = 0
+        index = 0
+        for column in columns:
+            distance += (instance[column] - center[index])**2
+            index += 1
+        return math.sqrt(distance)
 
-def analysis():
     df = pd.read_csv("norm_steam_games.csv")
     data = [item for (idx, item) in df.iterrows()]
 
-    print(data[0])
+    #print(data[0])
 
-    centers = clustering.lloyds(data, 1, ['norm_positive_rev', 'norm_Concurrent_Users'], n=10)
-    plot(data, centers, 'norm_positive_rev', 'norm_Concurrent_Users', scale=(0, 1))
+    k = 3
+    #columns = ['norm_positive_rev', 'norm_Concurrent_Users', 'norm_mean_forever_playtime']
+    columns = ['norm_positive_rev', 'norm_negative_rev']
 
-    print(centers)
+    centers = clustering.lloyds(data, k, columns, n=50)
+    plot(data, centers, 'norm_positive_rev', 'norm_negative_rev', scale=(0, 1))
+    #print(centers)
+
+    clusters = []
+    for i in range(k):
+        clusters.append([])
+
+    for instance in data:
+            smaller_index = 0
+            smaller_distance = dist(centers[smaller_index], instance, columns)
+
+            for current_index in range(1, len(centers)):
+                current_distance = dist(centers[current_index], instance, columns)
+
+                if current_distance < smaller_distance:
+                    smaller_distance = current_distance
+                    smaller_index = current_index
+            
+            clusters[smaller_index].append(instance)
+    
+    smallest_cluster = clusters[0]
+    min_value = len(clusters[0])
+    for i in range(1, len(clusters)):
+        if min_value > len(clusters[i]):
+            min_value = len(clusters[i])
+            smallest_cluster = clusters[i]
+    
+    for instances in smallest_cluster:
+        print(instances['name'])
+
+def analysis_medoids():
+    def compare_genre(a,b):
+        a_set = set()
+        b_set = set()
+
+        if (type(a['genre']) != float):
+            temp = a['genre'].split(", ")
+            a_set = set(temp)
+        
+        if (type(b['genre']) != float):
+            temp = b['genre'].split(", ")
+            b_set = set(temp)
+
+        if len(a_set) != 0 and len(b_set) != 0:
+            intersect = abs(len(a_set.intersection(b_set)))
+            union = abs(len(a_set.union(b_set)))
+
+            jaccard = intersect/union
+
+            return 1 - jaccard
+        else:
+            return 1
+
+    def compare_tags(a,b):
+        a_set = set(ast.literal_eval(a['tags']))
+        b_set = set(ast.literal_eval(b['tags']))
+
+        if len(a_set) != 0 and len(b_set) != 0:
+            intersect = abs(len(a_set.intersection(b_set)))
+            union = abs(len(a_set.union(b_set)))
+
+            jaccard = intersect/union
+
+            return 1 - jaccard
+        else:
+            return 1
+    
+    def compare_tags_genre(a,b):
+        return(compare_genre(a,b) + compare_tags(a,b))/2.0
+
+    df = pd.read_csv("norm_steam_games.csv")
+    data = [item for (idx, item) in df.iterrows()]
+
+    k = 4
+
+    centers = clustering.kmedoids(data, k, compare_genre, n=3)
+
+    clusters = []
+    for i in range(k):
+        clusters.append([])
+
+    for instance in data:
+        smaller_index = 0
+        smaller_distance = compare_genre(centers[smaller_index], instance)
+
+        for current_index in range(1, len(centers)):
+            current_distance = compare_genre(centers[current_index], instance)
+
+            if current_distance < smaller_distance:
+                smaller_distance = current_distance
+                smaller_index = current_index
+            
+        clusters[smaller_index].append(instance)
+
+    smallest_cluster = clusters[0]
+    min_value = len(clusters[0])
+    for i in range(1, len(clusters)):
+        if min_value > len(clusters[i]):
+            min_value = len(clusters[i])
+            smallest_cluster = clusters[i]
+    
+    for instances in smallest_cluster:
+        print(instances['name'])
+    
+    print(len(smallest_cluster))
+
+def main():
+    analysis_lloyds()
+    #analysis_medoids()
 
 if __name__ == "__main__":
-    analysis()
+    main()
--- clustering.py
+++ clustering.py
@@ -25,12 +25,12 @@ def lloyds(data, k, columns, centers=None, n=None, eps=None):
         return average
 
     def calc_threshold(previous_centers, current_centers, columns):
-        threshold = []
+        threshold = 0
         for i in range(len(previous_centers)):
             distance = 0
             for j in range(len(columns)):
                 distance += (current_centers[i][j] - previous_centers[i][j])**2
-            threshold.append(math.sqrt(distance))
+            threshold += distance
 
         return threshold
 
@@ -79,9 +79,9 @@ def lloyds(data, k, columns, centers=None, n=None, eps=None):
         if eps is not None:
             threshold = calc_threshold(previous_centers, centers, columns)
             #print(threshold)
-            for item in threshold:
-                if item < eps:
-                    check = False
+            if threshold < eps:
+                #print("exited through eps")
+                check = False
         
         iteration += 1
 
@@ -91,5 +91,74 @@ def lloyds(data, k, columns, centers=None, n=None, eps=None):
 def kmedoids(data, k, distance, centers=None, n=None, eps=None):
 # DO NOT CHANGE THE PRECEDING LINE
     # This function has to return a list of k cluster centroids (data instances!)
-    pass
+    def new_center(cluster, distance):
+        distance_sums = []
+        for instance in cluster:
+            summed = 0
+            for other_instance in cluster:
+                if instance.equals(other_instance):
+                    continue
+                else:
+                    summed += distance(instance, other_instance)
+            distance_sums.append(summed)
+        
+        next_center = distance_sums.index(min(distance_sums))
 
+        return cluster[next_center]
+        
+    def calc_threshold(previous_centers, current_centers, distance):
+        threshold = 0
+        index = 0
+        for instance in previous_centers:
+            threshold += distance(instance, current_centers[index])
+            index += 1
+
+        return threshold
+
+    clusters = []
+    for i in range(k):
+        clusters.append([])
+
+    if centers is None:
+        centers = []
+        random.seed()
+        for i in range(k):
+            random_instance = random.choice(data)
+            centers.append(random_instance)
+
+    if n is None:
+        n = 10
+
+    check = True
+    iteration = 0
+
+    while(iteration < n and check):
+        for instance in data:
+            smaller_index = 0
+            smaller_distance = distance(centers[smaller_index], instance)
+
+            for current_index in range(1, len(centers)):
+                current_distance = distance(centers[current_index], instance)
+
+                if current_distance < smaller_distance:
+                    smaller_distance = current_distance
+                    smaller_index = current_index
+            
+            clusters[smaller_index].append(instance)
+
+        previous_centers = centers.copy()
+        
+        for i in range(k):
+            if(len(clusters[i])):
+                centers[i] = new_center(clusters[i], distance)
+                clusters[i].clear()
+
+        if eps is not None:
+            threshold = calc_threshold(previous_centers, centers, distance)
+            #print(threshold)
+            if threshold < eps:
+                check = False
+        
+        iteration += 1
+
+    return centers
